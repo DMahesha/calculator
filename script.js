92bfaa1b24827602463b93bc34eff306e54d5db7
DMahesha <84167234+DMahesha@users.noreply.github.com>
2023-10-05 12:21:17 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,20 @@
 let calcDisplay = document.querySelector('#calc-display')
-const calcOne = document.querySelector('#calc-one')
-const calcTwo = document.querySelector('#calc-two')
-const calcThree = document.querySelector('#calc-three')
-const calcFour = document.querySelector('#calc-four')
-const calcFive = document.querySelector('#calc-five')
-const calcSix = document.querySelector('#calc-six')
-const calcSeven = document.querySelector('#calc-seven')
-const calcEight = document.querySelector('#calc-eight')
-const calcNine = document.querySelector('#calc-nine')
-const calcZero = document.querySelector('#calc-zero')
-const calcAdd = document.querySelector('#calc-add')
-const calcSub = document.querySelector('#calc-sub')
-const calcMul = document.querySelector('#calc-mul')
-const calcDiv = document.querySelector('#calc-div')
-const calcPeriod = document.querySelector('#calc-period')
 const calcEqual = document.querySelector('#calc-equal')
+const oprt = document.querySelectorAll('.oprt')
+const num = document.querySelectorAll('.num')
+const calcReset = document.querySelector('#calc-reset')
 
 let firstNumber = ''
 let firstOperator = ''
 let secondNumber = ''
 
-calcOne.addEventListener('click', pushOne)
-
-function pushOne() {
-    //firstNumber = 1
-    calcDisplay.textContent = 1
-    calcOne.style.cssText = 'background-color:red'
-    pushNumber(1)
-}
-
-calcAdd.addEventListener('click', pushAdd)
-
-function pushAdd() {
-    firstOperator = '+'
-    calcDisplay.textContent = '+'
-    calcAdd.style.cssText = 'background-color:yellow'
-}
-
-calcTwo.addEventListener('click', pushTwo)
-
-function pushTwo() {
-    //secondNumber = 2
-    calcDisplay.textContent = 2
-    calcTwo.style.cssText = 'background-color:green'
-    pushNumber(2)
-}
-
-calcEqual.addEventListener('click', evaluate)
+num.forEach(item => {
+    item.addEventListener('click', (e => {
+        let selNum = Number(e.target.textContent)
+        calcDisplay.textContent = selNum
+        pushNumber(selNum)
+    }))
+})
 
 function pushNumber(num) {
     if (firstNumber == '') {
@@ -56,8 +24,32 @@ function pushNumber(num) {
     }
 }
 
+oprt.forEach(item => {
+    item.addEventListener('click', (e => {
+        firstOperator = e.target.textContent
+        calcDisplay.textContent = firstOperator
+    }))
+});
+
+calcReset.addEventListener('click', resetCalc)
+
+function resetCalc() {
+    firstNumber = '';
+    firstOperator = '';
+    secondNumber = '';
+    calcDisplay.textContent = 0
+}
+
+calcEqual.addEventListener('click', evaluate)
+
 function evaluate() {
     if (firstOperator == '+') {
         calcDisplay.textContent = firstNumber + secondNumber
+    } else if (firstOperator == '-') {
+        calcDisplay.textContent = firstNumber - secondNumber
+    } else if (firstOperator == '*') {
+        calcDisplay.textContent = firstNumber * secondNumber
+    } else if (firstOperator == '/') {
+        calcDisplay.textContent = firstNumber / secondNumber
     }
 }
